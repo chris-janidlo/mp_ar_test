25b6714380fa139ead84154ec446b0da6d098977
crassSandwich <christopherjanidlo@gmail.com>
2018-10-15 20:38:51 -0400

    Skeleton of flow

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour 
+{
+	void Start ()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	public void LoadScene (string scene)
+	{
+		SceneManager.LoadScene(scene);
+	}
+}
